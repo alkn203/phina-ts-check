5938087285f4ddc51d9e79e3523da9e38f20c032
alkn203 <alkn203@gmail.com>
2023-02-11 00:24:43 +0900

    update

--- main.js
+++ main.js
@@ -18,10 +18,6 @@ const ASSETS = {
     'pieces': 'assets/pieces.png',
   },
 };
-// グリッド
-const grid = Grid(SCREEN_WIDTH, PIECE_NUM_X);
-// ピースグループ
-const pieceGroup = DisplayElement();
 // メインシーン
 phina.define('MainScene', {
   superClass: 'DisplayScene',
@@ -31,89 +27,84 @@ phina.define('MainScene', {
     this.superInit();
     // 背景色
     this.backgroundColor = 'black';
-    this.addChild(pieceGroup);
+    // グリッド
+    this.grid = Grid(SCREEN_WIDTH, PIECE_NUM_X);
+    // ピースグループ
+    this.pieceGroup = DisplayElement().addChildTo(this);
     // ピース配置
-    createPiece();
+    this.createPiece();
+  },
+  // ピース配置関数
+  createPiece: function () {
+    PIECE_NUM.times(function(/** @type {number} */ i) {
+      // グリッド配置用のインデックス値算出
+      const sx = i % PIECE_NUM_X;
+      const sy = Math.floor(i / PIECE_NUM_X);
+      // 番号
+      const num = i + 1;
+      // ピース作成
+      const piece = Piece(num).addChildTo(this.pieceGroup);
+      // Gridを利用して配置
+      piece.x = this.grid.span(sx) + PIECE_OFFSET;
+      piece.y = this.grid.span(sy) + PIECE_OFFSET;
+      // グリッド上のインデックス値
+      piece.indexPos = Vector2(sx, sy);
+      // タッチを有効にする
+      piece.setInteractive(true);
+      // タッチされた時の処理
+      piece.on('pointend', () => {
+        // ピース移動処理
+        this.movePiece(piece);
+      });
+      // 16番のピースは非表示
+      if (num === 16) {
+        piece.hide();
+      }
+    }, this);
+  },
+  // 16番ピース（空白）を取得
+  getBlankPiece: function () {
+    let result = null;
+    this.pieceGroup.children.some(function(piece) {
+      // 16番ピースを結果に格納I
+      if (piece.num === 16) {
+        result = piece;
+        return true;
+      }
+    });
+    return result;
+  },
+  // ピースの移動処理
+  movePiece: function (piece) {
+    // 空白ピースを得る
+    const blank = this.getBlankPiece();
+    // x, yの座標差の絶対値
+    const dx = Math.abs(piece.indexPos.x - blank.indexPos.x);
+    const dy = Math.abs(piece.indexPos.y - blank.indexPos.y);
+    // 隣り合わせの判定
+    if ((piece.indexPos.x === blank.indexPos.x && dy === 1) ||
+      (piece.indexPos.y === blank.indexPos.y && dx === 1)) {
+      // タッチされたピース位置を記憶
+      const tPos = Vector2(piece.x, piece.y);
+      // ピース移動処理
+      piece.tweener
+           .to({x:blank.x, y:blank.y}, 100)
+           .call(function() {
+             blank.setPosition(tPos.x, tPos.y);
+             piece.indexPos = this.coordToIndex(piece.position);
+             blank.indexPos = this.coordToIndex(blank.position);
+           }, this)
+           .play();
+    }
+  },
+  // 座標値からインデックス値へ変換
+  coordToIndex: function (vec) {
+    const x = Math.floor(vec.x / PIECE_SIZE);
+    const y = Math.floor(vec.y / PIECE_SIZE);
+    return Vector2(x, y);
   },
 });
-// 16番ピース（空白）を取得
-function getBlankPiece() {
-  /** @type { Piece | null} */
-  let result = null;
-
-  pieceGroup.children.some(function(piece) {
-    // 16番ピースを結果に格納I
-    if (piece.num === 16) {
-      result = piece;
-      return true;
-    }
-  });
-  return result;
-};
-// ピースの移動処理
-function movePiece(piece) {
-  // 空白ピースを得る
-  const blank = getBlankPiece();
-  // x, yの座標差の絶対値
-  const dx = Math.abs(piece.indexPos.x - blank.indexPos.x);
-  const dy = Math.abs(piece.indexPos.y - blank.indexPos.y);
-  // 隣り合わせの判定
-  if ((piece.indexPos.x === blank.indexPos.x && dy === 1) ||
-     (piece.indexPos.y === blank.indexPos.y && dx === 1)) {
-    // タッチされたピース位置を記憶
-    const tPos = Vector2(piece.x, piece.y);
-    // ピース移動処理
-    piece.tweener
-         .to({x:blank.x, y:blank.y}, 100)
-         .call(function() {
-            blank.setPosition(tPos.x, tPos.y);
-            piece.indexPos = coordToIndex(piece.position);
-            blank.indexPos = coordToIndex(blank.position);
-         })
-         .play();
-  }
-};
-// ピース配置関数
-function createPiece() {
-  PIECE_NUM.times(function(/** @type {number} */ i) {
-    // グリッド配置用のインデックス値算出
-    const sx = i % PIECE_NUM_X;
-    const sy = Math.floor(i / PIECE_NUM_X);
-    // 番号
-    const num = i + 1;
-    // ピース作成
-    const piece = Piece(num).addChildTo(pieceGroup);
-    // Gridを利用して配置
-    piece.x = grid.span(sx) + PIECE_OFFSET;
-    piece.y = grid.span(sy) + PIECE_OFFSET;
-    // グリッド上のインデックス値
-    piece.indexPos = Vector2(sx, sy);
-    // タッチを有効にする
-    piece.setInteractive(true);
-    // タッチされた時の処理
-    piece.on('pointend', () => {
-      // ピース移動処理
-      movePiece(piece);
-    });
-    // 16番のピースは非表示
-    if (num === 16) {
-      piece.hide();
-    }
-  });
-};
-// 座標値からインデックス値へ変換
-function coordToIndex (vec) {
-  const x = Math.floor(vec.x / PIECE_SIZE);
-  const y = Math.floor(vec.y / PIECE_SIZE);
-  return Vector2(x, y);
-};
 // ピースクラス
-/**
- * @typedef {Object} Piece
- * @property {number} num
- * @property {number} frameIndex
- * @property {Vector2} indexPos
- */
 phina.define('Piece', {
   // Spriteを継承
   superClass: 'Sprite',
